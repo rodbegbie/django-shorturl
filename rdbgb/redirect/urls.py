7186991e34749abd5715000bf34f51fa17810ca2
Rod Begbie <rod@arsecandle.org>
2007-11-24 02:54:05 +0000

    Moved index and robots.txt to template files

--- rdbgb/redirect/urls.py
+++ rdbgb/redirect/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('rdbgb.redirect.views',
-    (r'^$', 'about'),
-    (r'^robots.txt$', 'robots'),
+urlpatterns = patterns('',)
+
+urlpatterns += patterns('rdbgb.redirect.views',
     (r'^(?P<key>[a-zA-Z0-9]{1,10})/', 'redirect'),    
 )
 
-
+urlpatterns += patterns('django.views.generic.simple',
+    (r'^$',             'direct_to_template', {'template': 'index.html'}),
+    (r'^robots.txt$',   'direct_to_template', {'template': 'robots.txt'}),   
+)
--- rdbgb/redirect/views.py
+++ rdbgb/redirect/views.py
@@ -3,14 +3,6 @@ from django.http import HttpResponsePermanentRedirect
 from django.shortcuts import get_object_or_404
 from models import *
 
-def about(request):
-    return HttpResponse("Hello, world. You're at Rod Begbie's redirection server.")
-
-def robots(request):
-    return HttpResponse("""# No robots allowed
-User-agent: *
-Disallow: /""")
-
 def redirect(request, key):
     target = get_object_or_404(Target, key=key)
     
