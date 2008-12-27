d50f0d26a6b2de3e671f2be5f177b8541625f235
Rod Begbie <rod@arsecandle.org>
2008-12-26 18:55:28 -0800

    Switch URL order

--- rdbgb/redirect/urls.py
+++ rdbgb/redirect/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',)
 
-urlpatterns += patterns('rdbgb.redirect.views',
-    (r'^(?P<key>[a-zA-Z0-9]{1,10})[\,\)]*[\./]', 'redirect'),    
-)
-
 urlpatterns += patterns('django.views.generic.simple',
     (r'^$',             'direct_to_template', {'template': 'index.html'}),
     (r'^robots.txt$',   'direct_to_template', {'template': 'robots.txt'}),   
+)
+
+urlpatterns += patterns('rdbgb.redirect.views',
+    (r'^(?P<key>[a-zA-Z0-9]{1,10})[\,\)]*[\./]', 'redirect'),    
 )
